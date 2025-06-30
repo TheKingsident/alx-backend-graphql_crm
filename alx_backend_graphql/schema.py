a271d9b67240db9265d2c0c81907d3363c07b1e7
TheKingsident <kingsident@gmail.com>
2025-06-30 14:04:09 +0100

    Enhance Query class documentation for GraphQL API

--- alx_backend_graphql/schema.py
+++ alx_backend_graphql/schema.py
@@ -2,6 +2,9 @@ import graphene
 from graphene_django.types import DjangoObjectType
 
 class Query(graphene.ObjectType):
+    """
+    Root query for the GraphQL API.
+    """
     hello = graphene.String(default_value="Hello, GraphQL!")
 
 schema = graphene.Schema(query=Query)
